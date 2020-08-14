966ba5b5cf3c1a10f1c9fe8cb8d5b15d248844a4
Shen Liao <shen.liao@thoughtworks.com>
2020-08-14 17:54:09 +0800

    Connection to backend succeed,

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import './navigation.css'
 import {BrowserRouter, Link, Switch, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <nav className = "navigation">
-        <BrowserRouter>
-          <Link to= '/'>商城</Link>
-          <Link to='/orders'>订单</Link>
-          <Link to='/addItems'>添加商品</Link>
+class App extends Component {
+  state = {
+      items: [],
+      orders: [],
+  }
 
-          <Switch>
-            <Route path='/orders' />
-            <Route path='/addItems' />
-            <Route exact path='/' />
-          </Switch>
-        </BrowserRouter>       
-      </nav>
-    </div>
-  );
+  componentDidMount() {
+    const itemUrl = 'http://localhost:8080/shop/item';
+    const orderUrl = 'http://localhost:8080/shop/order'
+    const items = [];
+    const orders = [];
+
+    fetch(itemUrl).then((response) => {
+      if(response.status === 200) {
+        return response.json();
+      }
+      return Promise.reject('error');
+    }).then(item => items.add(item))
+      .catch(console.log('error'));
+
+    fetch(orderUrl).then((response) => {
+      if(response.status === 200) {
+        return response.json();
+      }
+      return Promise.reject('error');
+    }).then(order => orders.add(order))
+      .catch(console.log('error'));
+
+      this.setState({
+        items,orders,
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <nav className = "navigation">
+          <BrowserRouter>
+            <Link className='pages' to= '/'>商城</Link>
+            <Link className='pages' to='/orders'>订单</Link>
+            <Link className='pages' to='/addItems'>添加商品</Link>
+
+            <Switch>
+              <Route path='/orders' />
+              <Route path='/addItems' />
+              <Route exact path='/' />
+            </Switch>
+          </BrowserRouter>       
+        </nav>
+      </div>
+    );
+  };
 }
 
 export default App;
